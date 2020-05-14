2c8a7ad5200dd286eead582b98d44b4500fa7a8e
maikalscastanys <miquicm@gmail.com>
2020-05-14 20:26:45 +0200

    Added MainActivity

--- app/src/main/java/com/example/catantournament/ui/main_activity/MainActivity.kt
+++ app/src/main/java/com/example/catantournament/ui/main_activity/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.catantournament.ui.main_activity
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import com.example.catantournament.ui.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setFragmentContent(MainFragment.newInstance())
     }
 }
---
+++ app/src/main/java/com/example/catantournament/ui/main_activity/MainFragment.kt
@@ -0,0 +1,44 @@
+package com.example.catantournament.ui.main_activity
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.catantournament.R
+import com.example.catantournament.ui.player_list.PlayerListFragment
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_main, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setBottomNavigationView()
+    }
+
+    private fun setBottomNavigationView() {
+        bottom_navigation_View.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menu_item_player_list -> openPlayerListFragment()
+            }
+            false
+        }
+        bottom_navigation_View.selectedItemId = R.id.menu_item_player_list
+    }
+
+    private fun openPlayerListFragment() {
+        childFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, PlayerListFragment.newInstance()).commit()
+    }
+
+    companion object {
+        const val TAG = "MainFragment"
+        fun newInstance() = MainFragment()
+    }
+}
